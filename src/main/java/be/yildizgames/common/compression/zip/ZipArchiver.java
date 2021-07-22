02e795d5f26c908cd01d9265d6d46c302fa53738
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2021-07-22 20:19:01 +0200

    [FEATURE] Zip archiving.

--- src/main/java/be/yildizgames/common/compression/zip/ZipArchiver.java
+++ src/main/java/be/yildizgames/common/compression/zip/ZipArchiver.java
@@ -26,7 +26,14 @@ package be.yildizgames.common.compression.zip;
 
 import be.yildizgames.common.compression.Archiver;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @author Grégory Van den Borre
@@ -35,6 +42,33 @@ public class ZipArchiver implements Archiver {
 
     @Override
     public final void pack(Path file, Path destination) {
-        //FIXME implements.
+        try(var zos = new ZipOutputStream(Files.newOutputStream(destination))) {
+            zipDirectory(file.toFile(), file.toFile().getName(), zos);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void zipDirectory(File folder, String parentFolder,
+                              ZipOutputStream zos) throws IOException {
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                zipDirectory(file, parentFolder + "/" + file.getName(), zos);
+                continue;
+            }
+            doZip(parentFolder + "/" + file.getName(), file, zos);
+        }
+    }
+
+    private void doZip(String name, File file, ZipOutputStream zos) throws IOException {
+        zos.putNextEntry(new ZipEntry(name));
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(
+                file));
+        byte[] bytesIn = new byte[4096];
+        int read;
+        while ((read = bis.read(bytesIn)) != -1) {
+            zos.write(bytesIn, 0, read);
+        }
+        zos.closeEntry();
     }
 }
